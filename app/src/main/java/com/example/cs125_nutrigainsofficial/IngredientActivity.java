1598e06f821a9b6c0b98ea4969a0531ccb570075
Rahul Pendyala <rahul.pen24@gmail.com>
2019-03-17 11:09:49 -0700

    basic ingredient add changes

--- app/src/main/java/com/example/cs125_nutrigainsofficial/IngredientActivity.java
+++ app/src/main/java/com/example/cs125_nutrigainsofficial/IngredientActivity.java
@@ -1,5 +1,7 @@
 package com.example.cs125_nutrigainsofficial;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,17 +10,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class IngredientActivity extends AppCompatActivity {
     ArrayList<String> ingredientItemsList =new ArrayList<>();
     ArrayAdapter<String> adapter;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseMessages = database.getReference("message");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient);
+        FirebaseApp.initializeApp(this);
     }
 
     @Override
@@ -41,5 +54,7 @@ public class IngredientActivity extends AppCompatActivity {
             };
 
         });
-     }
+
+        }
+
 }
