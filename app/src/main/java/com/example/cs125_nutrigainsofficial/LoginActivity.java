f241a3d271f71496a0767c4d73934dfdcb03b0cc
Patrick Mang <pmang@uci.edu>
2019-03-18 22:49:13 -0700

    Updated code

--- app/src/main/java/com/example/cs125_nutrigainsofficial/LoginActivity.java
+++ app/src/main/java/com/example/cs125_nutrigainsofficial/LoginActivity.java
@@ -17,11 +17,8 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 //import android.support.design.widget.Snackbar;
 
